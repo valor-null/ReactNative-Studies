bffbe3f933e38de40fde4fb602dc6422adc0c569
valor-null <valeriaconcedsantos@gmail.com>
2025-10-14 20:46:48 -0300

    estilização da camera

--- app/camera.jsx
+++ app/camera.jsx
@@ -1,6 +1,7 @@
 import { CameraView, useCameraPermissions } from "expo-camera";
 import React, { useRef, useState } from "react";
-import { Button, Image, Text, View } from "react-native";
+import { Button, Image, Pressable, StyleSheet, Text, View } from "react-native";
+
 
 // 1) Instale antes: npx expo install expo-camera
 
@@ -38,22 +39,65 @@ export default function App() {
   // Se já tem foto, mostra a imagem
   if (photoUri) {
     return (
-      <View>
-        <Image source={{ uri: photoUri }} style={{ width: 300, height: 400 }} />
-        <Button title="Tirar outra" onPress={() => setPhotoUri(null)} />
+      <View style={cam.container}>
+        <Image
+        source={require("../assets/gif/camera.png")} 
+        style={cam.logo}
+        resizeMode="contain" 
+        /> 
+        
+        <Image source={{ uri: photoUri }} style={{ width: 300, height: 300, borderRadius: 24 }} />
+        <Pressable style={cam.btN} onPress={() => setPhotoUri(null)  }>
+          <Text style={cam.text}>Tirar outra</Text>
+        </Pressable>
       </View>
     );
   }
 
   // Senão, mostra a câmera + botão de captura
   return (
-    <View>
+    <View style={cam.container}>
+        <Image
+        source={require("../assets/gif/camera.png")} 
+        style={cam.logo}
+        resizeMode="contain" 
+        /> 
       <CameraView
         ref={cameraRef}
-        style={{ width: 300, height: 400 }} // tamanho mínimo pra aparecer
+        style={{ width: 300, height: 300, borderRadius: 24 }} // tamanho mínimo pra aparecer
         facing="back"                       // câmera traseira
       />
-      <Button title="Tirar foto" onPress={takePhoto} />
+      <Pressable onPress={takePhoto} style={cam.btN}>
+        <Text style={cam.text}> Tirar foto </Text>
+      </Pressable>
+
     </View>
   );
 }
+
+const cam = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#181322",
+  },
+  btN: {
+    width: 300,
+    height: 40,
+    margin: 10,
+    backgroundColor: "#000000",
+    borderRadius: 12,
+    padding: 4
+  },
+  text: {
+    textAlign: "center",
+    color: "#664eeb",
+    fontSize: 22,
+    fontWeight: 700,
+  },
+  logo: {
+    width: 150,
+    height: 100
+  }
+})
