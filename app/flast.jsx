88b62f7468de733b41393e2d9e7e824a97db7b84
valor-null <valeriaconcedsantos@gmail.com>
2025-10-16 11:34:57 -0300

    flatlist desing

--- app/flast.jsx
+++ app/flast.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App(){
     // lista inicial
@@ -10,6 +10,7 @@ export default function App(){
         "Laranja",
         "Uva",
         "Manga",
+        "Lichia",
     ]);
 
       // demonstração de "puxar p/ atualizar"
@@ -24,25 +25,34 @@ export default function App(){
 };
 
   // como desenhar cada item
-    const renderItem = ({ item }) => <Text>- {item}</Text>;
+    const renderItem = ({ item }) => <Text style={f.h2}>- {item}</Text>;
 
 return (
     <View style={f.container}>
-        <Text>Minhas frutas:</Text>
+
+        <Image
+        source={require("../assets/gif/strawberry.png")} // local aonde ta a imagem
+        style={f.logo}
+        resizeMode="contain" // “encaixe a imagem inteira dentro do retângulo sem cortar mantando a proporção 
+        accessibilityLabel="Garota mexendo no computador" // texto de acessibilidade
+        />
+
+        <Text style={f.h1}>Lista de Frutas (FlatList) </Text>
 
         <FlatList
+        style={f.lista}
         data={frutas}                         // a fonte de dados
         renderItem={renderItem}               // como desenhar cada linha
         keyExtractor={(item) => item}         // chave única (aqui uso o próprio texto)
-        ListEmptyComponent={<Text>Sem itens.</Text>}
+        ListEmptyComponent={<Text style={f.h2} >Sem itens.</Text>}
         refreshing={refreshing}               // mostra o estado de "atualizando"
         onRefresh={onRefresh}                 // puxa de cima para atualizar
         />
 
-        <Button
-        title="Limpar lista"
-        onPress={() => setFrutas([])}         // zera a lista para ver o Empty
-        />
+        <Pressable style={f.btn} onPress={() => setFrutas([])}> 
+            <Text style={f.text}> Limpar lista </Text> 
+        </Pressable>
+
     </View>
     );
 }
@@ -50,9 +60,43 @@ return (
 const f = StyleSheet.create({
     container: {
         flex: 1, 
-        padding: 100,
-        justifyContent: "flex-start", 
+        padding: 120,
+        justifyContent: "center", 
         alignItems: "center", 
-        backgroundColor: "#FFCFCF"
+        backgroundColor: "#181322",
+    },
+     btn: {
+        borderRadius: 10,
+        backgroundColor: "#000",
+        borderColor: "#000",
+        borderWidth: 2,
+    },
+    text: {
+        color: "#db3264",
+        textAlign: "center",
+        padding: 20,
+        fontWeight: 700,
+        fontSize: 15
+    },
+    h1: {
+        color: "#ff268b",
+        fontSize: 25,
+        width: 290,
+        fontWeight: 700,
+    },
+    lista: {
+        fontSize: 25,
+        padding: 20,
+        width: 300,
+        margin: 10,
+    },
+    h2: {
+        textAlign: "left",
+        fontSize: 30,
+        color: "#ff22cf",
+    }, 
+    logo: {
+        width: 200,
+        height: 200
     }
 })
